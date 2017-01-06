d432185c728b5a6d5fd374f5d3bea1840a42d4a2
Betty <yuanxiaoli301@gmail.com>
2017-01-07 06:39:36 +0800

    提交第12 和第13 个练习

---
+++ ex13.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+script, first, second, third, fourth = argv
+
+t = raw_input()
+d = raw_input()
+
+
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your fourth variable is:", fourth
+print "betty input:",t
+print "betty input again:", d
